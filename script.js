d9220e0a2b3b35d44295d007f5fe8c4ba21d2222
ErickDurval1 <127344908+ErickDurval1@users.noreply.github.com>
2023-06-27 15:08:32 -0300

    corrigindo erros do javascript.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let musicas = [
-    {titulo:'memories', artista:'Conan gray', source:'Áudio do WhatsApp de 2023-06-21 à(s) 16.53.46.mp3', img:'01656072193.webp'},
+    {titulo:'memories', artista:'Conan gray', source:'musicas/Áudio do WhatsApp de 2023-06-21 à(s) 16.53.46.mp33', img:'imagens/01656072193.webp'},
     {titulo:'Anunciação', artista:'Bossa Nova Brasil', source:'Áudio do WhatsApp de 2023-06-22 à(s) 10.38.16.mp3', img:'Imagem do WhatsApp de 2023-06-22 à(s) 10.27.53.jpg'},
     {titulo:'Preciso me encontrar', artista:'Cartola', source:'Áudio do WhatsApp de 2023-06-22 à(s) 10.30.05precisomeencontrar.mp3', img:'Imagem do WhatsApp de 2023-06-22 à(s) 10.25.06cartola.jpg'},
     {titulo:'Carolina', artista:'Vinicius de moraes', source:'Áudio do WhatsApp de 2023-06-22 à(s) 10.38.13carolina.mp3', img:'Imagem do WhatsApp de 2023-06-22 à(s) 10.24.36carlina.jpg'},
